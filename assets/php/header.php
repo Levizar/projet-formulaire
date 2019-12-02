aab729fe652d43762a6e94e3c4f5f11338cde443
Térence <terencehecq@gmail.com>
2019-12-02 14:19:04 +0100

    Active on nav bar

--- assets/php/header.php
+++ assets/php/header.php
@@ -1,21 +1,36 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="index.php">Hackers Poulette</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+<!DOCTYPE html>
+<html lang="fr">
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav col-12 d-flex justify-content-around">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">Accueil <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="products.php">Produits</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="contact.php">Contactez-nous</a>
-      </li>
-    </ul>
-  </div>
-  <div class="col-4"></div>
-</nav>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Hackers poulette</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="index.php">Hackers Poulette</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav col-12 d-flex justify-content-around">
+                <li class="nav-item <?php if($page == "accueil") echo "active";?>">
+                    <a class="nav-link" href="index.php">Accueil <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item <?php if($page == "products") echo "active";?>">
+                    <a class="nav-link" href="products.php">Produits</a>
+                </li>
+                <li class="nav-item <?php if($page == "contact") echo "active";?>">
+                    <a class="nav-link" href="contact.php">Contactez-nous</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-4"></div>
+    </nav>
