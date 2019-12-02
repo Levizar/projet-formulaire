f4b58b2773b626734dd490b2865900cc7fc68e47
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2019-12-02 22:09:44 +0100

    footer.php reponsiv class + comments

--- assets/php/footer.php
+++ assets/php/footer.php
@@ -1,11 +1,15 @@
     <footer class="py-1">
-        <ul class="row container-fluid mb-0">
-            <li class="col-1 text-center"><a href=""><i class="fab fa-tripadvisor fa-lg"></i></a></li>
-            <li class="col-1 text-center"><a href=""><i class="fab fa-facebook-square fa-lg"></i></a></li>
-            <li class="col-1 text-center"><a href=""><i class="fab fa-instagram fa-lg"></i></a></li>
-            <li class="col-1 text-center"><a href=""><i class="fab fa-twitter-square fa-lg"></i></a></li>
-            <li class="col-1 text-center"><a href=""><i class="fab fa-jedi-order fa-lg"></i></a></li>
-            <li class="col-4 ml-auto text-right">©2019 Hackers Poulette</li>
+        <ul class="row container-fluid mb-0 mr-0 justify-content-around">
+        <!-- rempalcement des col-1 par col-sm-1 pour préserver la version pc -->
+        <!-- ajout de col-4 pour avoir plusieurs lignes en mobile -->
+            <li class="col-4 col-sm-1 text-center"><a href=""><i class="fab fa-tripadvisor fa-lg"></i></a></li>
+            <li class="col-4 col-sm-1 text-center"><a href=""><i class="fab fa-facebook-square fa-lg"></i></a></li>
+            <li class="col-4 col-sm-1 text-center"><a href=""><i class="fab fa-instagram fa-lg"></i></a></li>
+            <!-- Remplacement text-center par text-sm-center pour préserver version pc -->
+            <!-- Ajout de text-right/left + mr/ml-5 pour centrer sur 2eme ligne -->
+            <li class="col-4 col-sm-1 mr-5 mr-sm-0 text-right text-sm-center"><a href=""><i class="fab fa-twitter-square fa-lg"></i></a></li>
+            <li class="col-4 col-sm-1 ml-5 ml-sm-0 text-left text-sm-center"><a href=""><i class="fab fa-jedi-order fa-lg"></i></a></li>
+            <li class="col-12 col-sm-6 ml-sm-auto text-center text-sm-right">©2019 Hackers Poulette</li>
         </ul>
     </footer>
 
