3b7928a219df7ac3a9f18b1fde9bf5500ff86a96
Térence <terencehecq@gmail.com>
2019-12-02 14:04:40 +0100

    header in progress

--- contact.php
+++ contact.php
@@ -4,11 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hackers poulette</title>
+    <title>Document</title>
 </head>
 <body>
-    
-<script src="assets/php/header.php"></script>
     
 </body>
 </html>
