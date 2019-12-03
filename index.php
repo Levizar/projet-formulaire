e2ecabce71f5678a05f72433f2d7b8c994a42399
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2019-12-03 11:54:13 +0100

    index.php jumbotron my

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $page = "accueil";
 
 include 'assets/php/header.php';?>
 
-<div class="container">
+<div class="container my-3">
     <div class="jumbotron">
         <h1 class="display-4">Hello, world!</h1>
         <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
