998fa7cabca02b49e385bff23a026e238efc0285
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2019-12-04 12:09:48 +0100

    base sanitization function

--- assets/php/form.php
+++ assets/php/form.php
@@ -1,16 +1,32 @@
 <?php
-$firstName = $_POST['firstname'];
-$lastName = $_POST['lastname'];
-$gender = $_POST['gender'];
-$email = $_POST['email'];
-$country = $_POST['country'];
-$message = $_POST['message'];
-$honeyPot = $_POST['telephone'];
+echo '<pre>';
+print_r($_POST);
+echo gettype($_POST['model']);
 
-function form($firstName,$lastName,$gender,$email,$country,$message,$honeyPot){
-    // Si honeyPot rempli: ne rien faire
-    if(!isset($honeyPot)){
+
+
+// $data is $_POST
+function form($form_input){
+    // List of filters
+    $arr_filters = array
+    (
+        [firstname] => FILTER_SANITIZE_STRING,
+        [lastname] => FILTER_SANITIZE_STRING,
+        [gender] => FILTER_SANITIZE_STRING,
+        [email] => FILTER_VALIDATE_EMAIL,
+        [country] => FILTER_SANITIZE_STRING,
+        [message] => FILTER_SANITIZE_STRING,
+        [model] => FILTER_SANITIZE_STRING
+    );
+
+    // Si $_POST['model'] est un honeypot
+    // Si rempli: ne rien faire
+    if(!isset($_POST['model'])){
         // Sanitizer
-        
+        $sanitized_form = filter_input_array(INPUT_POST, $arr_filters);
+        echo print_r($sanitized_form);
+
+    } else {
+        // à faire renvoyer page de "validation OK" pour le bot suite au honeypot
     }
 }
