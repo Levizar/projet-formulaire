23a5776210fcfcb1827c54aa4d2d5503921bf2df
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2019-12-04 18:54:45 +0100

    form.php : redirection ok / still to do: put correct info into html

--- assets/php/form.php
+++ assets/php/form.php
@@ -1,5 +1,13 @@
 <?php
 
+// thx to stackoverflow
+function redirect($url) {
+    ob_start();
+    header('Location: '.$url);
+    ob_end_flush();
+    die();
+}
+
 function form_processing(){
     // List of filters
     $arr_sanitizers = [
@@ -20,7 +28,7 @@ function form_processing(){
         'email' => FILTER_VALIDATE_EMAIL,
         'country' => FILTER_VALIDATE_REGEXP,
         'message' => FILTER_VALIDATE_REGEXP,
-        'model' => FILTER_VALIDATE_REGEXP 
+        // 'model' => FILTER_VALIDATE_REGEXP 
     ];
     
     // List of regexp for the validaters
@@ -31,7 +39,7 @@ function form_processing(){
         // 'email' => '/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/', not used beause filter_validate_email
         'country' => '/[\w-]{2,30}/',
         'message' => '/[\w\W]+/',
-        'model' => '//', 
+        // 'model' => '//', 
     ];
 
     $arr_errors = [
@@ -41,7 +49,7 @@ function form_processing(){
         'email' => null,
         'country' => null,
         'message' => null,
-        'model' => null 
+        // 'model' => null 
     ];
 
     // Sanitization of the form anwser before any operation
@@ -51,6 +59,8 @@ function form_processing(){
     if($sanitized_form['model'] == ""){
         // This variable become false only if at least one element is unvalid
         $is_form_valid = true;
+        // unset the model #HoneyPot
+        unset($sanitized_form['model']);
         
         // Validation
         foreach($sanitized_form as $key => $value){
@@ -63,13 +73,19 @@ function form_processing(){
                 )){
                 }else{
                     $arr_errors["$key"] = "$key n'est pas valide";
+                    echo '<pre>';
+                    print_r($arr_errors);
                     $is_form_valid = false;
                 }
             }
             if($is_form_valid){
                 // Action to do if all validation step passed
+                // redirection to the ok pg
+                redirect("../../valid-form.php");
+                
                 
             } else {
+                echo 'going to NOT valid';
                 // if the form isn't valid
                 // Send back users to the form
 
