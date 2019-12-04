b1a8da4aeb61fae33915b70b6a390c187fe15cc0
Térence <terencehecq@gmail.com>
2019-12-04 10:50:45 +0100

    couleurs boutons et liste déroulante pays

--- products.php
+++ products.php
@@ -7,9 +7,18 @@ include 'assets/php/header.php';
 include 'assets/php/card.php'; ?>
 
 
+<h1 class="text-center my-5">Nos produits</h1>
+
 <div class="container col-11 col-lg-9 d-flex flex-wrap justify-content-around">
 
+
 <?php generate_card("raspberryPi1.jpg","Raspberry pi 1", "Joli rasberry qui fonctionne super bien youhou", "#");?>
+<?php generate_card("raspberryPi2.jpg","Raspberry pi 2", "Un autre joli rasberry qui fonctionne super bien waiiiii", "#");?>
+<?php generate_card("raspberryPi2.jpg","Raspberry pi 2", "Un autre joli rasberry qui fonctionne super bien waiiiii", "#");?>
+<?php generate_card("raspberryPi2.jpg","Raspberry pi 2", "Un autre joli rasberry qui fonctionne super bien waiiiii", "#");?>
+<?php generate_card("raspberryPi2.jpg","Raspberry pi 2", "Un autre joli rasberry qui fonctionne super bien waiiiii", "#");?>
+<?php generate_card("raspberryPi2.jpg","Raspberry pi 2", "Un autre joli rasberry qui fonctionne super bien waiiiii", "#");?>
+<?php generate_card("raspberryPi2.jpg","Raspberry pi 2", "Un autre joli rasberry qui fonctionne super bien waiiiii", "#");?>
 <?php generate_card("raspberryPi2.jpg","Raspberry pi 2", "Un autre joli rasberry qui fonctionne super bien waiiiii", "#");?>
 <?php generate_card("raspberryPi2.jpg","Raspberry pi 2", "Un autre joli rasberry qui fonctionne super bien waiiiii", "#");?>
 
