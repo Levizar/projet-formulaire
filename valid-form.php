1f33d0ef365dc29491ddd9d04ddb91463c171271
Térence <terencehecq@gmail.com>
2019-12-04 18:29:52 +0100

    ajout untracked

---
+++ valid-form.php
@@ -0,0 +1,21 @@
+<?php
+$page = "form-validate";
+
+// <!----------- Header (with head an body tags) ----------->
+
+include 'assets/php/header.php';
+?>
+
+<div class="container d-flex align-items-center flex-column" class="col-10">
+    <img src="assets/img/checked.png" alt="validé" class="img-fluid mb-3" style="width:20%">
+    <h2 class="text-center">Votre demande a été envoyée avec succès !</h2>
+    <a href="index.php" class="btn bg-color my-5">Revenir au site</a>
+
+</div>
+
+
+
+
+<!----------- Footer (with end tags) ----------->
+
+<?php include 'assets/php/footer.php';?> 
