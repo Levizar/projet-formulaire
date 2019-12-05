88d3ee68feff26ba0e2cb67f6b18fec9fd043fcd
Térence <terencehecq@gmail.com>
2019-12-05 19:39:19 +0100

    Favicon & titre de page

--- assets/php/header.php
+++ assets/php/header.php
@@ -5,20 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="assets/img/favicon.ico" type="image/png"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/b534382f55.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="assets/css/style.css">
 
-    <title>Hackers poulette <?php switch ($page) {
+    <title>Hackers poulette - <?php switch ($page) {
         case 'accueil':
-            echo 'accueil';
+            echo 'Accueil';
             break;
         case 'products':
-            echo 'products';
+            echo 'Produits';
             break;
         case 'contact':
-            echo 'contact';
+            echo 'Contact';
             break;
         default:
             break;
@@ -29,7 +30,6 @@
 <body>
     <nav class="navbar sticky-top navbar-expand-sm navbar-dark bg-color mb-2">
         <a class="navbar-brand text-center d-sm-none font-weight-bolder" href="index.php">Hackers Poulette</a>
-	    <link rel="icon" href="assets/img/favicon.ico" type="image/png"/>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
