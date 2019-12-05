a759cc0287c74d14916d47acecfdd5e83becc5f1
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2019-12-05 09:58:27 +0100

    redirection on valid form ok - to do: working on not valid redirection

--- assets/php/form.php
+++ assets/php/form.php
@@ -1,14 +1,16 @@
 <?php
 
 // thx to stackoverflow
-function redirect($url) {
+function redirect($url)
+{
     ob_start();
-    header('Location: '.$url);
+    header('Location: ' . $url);
     ob_end_flush();
     exit();
 }
 
-function form_processing(){
+function form_processing()
+{
     // List of filters
     $arr_sanitizers = [
         'firstname' => FILTER_SANITIZE_STRING,
@@ -17,7 +19,7 @@ function form_processing(){
         'email' => FILTER_SANITIZE_EMAIL,
         'country' => FILTER_SANITIZE_STRING,
         'message' => FILTER_SANITIZE_STRING,
-        'model' => FILTER_SANITIZE_STRING
+        'model' => FILTER_SANITIZE_STRING,
     ];
 
     // List of validaters
@@ -28,9 +30,9 @@ function form_processing(){
         'email' => FILTER_VALIDATE_EMAIL,
         'country' => FILTER_VALIDATE_REGEXP,
         'message' => FILTER_VALIDATE_REGEXP,
-        // 'model' => FILTER_VALIDATE_REGEXP 
+        // 'model' => FILTER_VALIDATE_REGEXP
     ];
-    
+
     // List of regexp for the validaters
     $arr_regexp_validers = [
         'firstname' => '/[\w-]{2,20}/',
@@ -39,7 +41,7 @@ function form_processing(){
         // 'email' => '/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/', not used beause filter_validate_email
         'country' => '/[\w-]{2,30}/',
         'message' => '/[\w\W]+/',
-        // 'model' => '//', 
+        // 'model' => '//',
     ];
 
     $arr_errors = [
@@ -49,51 +51,56 @@ function form_processing(){
         'email' => null,
         'country' => null,
         'message' => null,
-        // 'model' => null 
+        // 'model' => null
     ];
 
     // Sanitization of the form anwser before any operation
     $sanitized_form = filter_input_array(INPUT_POST, $arr_sanitizers);
-    
+
     // Checking if the honeypot is still intact
-    if($sanitized_form['model'] == ""){
+    if ($sanitized_form['model'] == "") {
         // This variable become false only if at least one element is unvalid
         $is_form_valid = true;
         // unset the model #HoneyPot
         unset($sanitized_form['model']);
-        
-        // Validation
-        foreach($sanitized_form as $key => $value){
-            // Erasing space before and after the answers
+
+        // (un)Validation step
+        foreach ($sanitized_form as $key => $value) {
+            // Erasing space char before and after the answers
             $sanitized_form["$key"] = trim($value);
-            if( filter_var(
-                $value, 
+            // If the data aren't validate, prepare an error message for rejection
+            if (! filter_var(
+                $value,
                 $arr_filters_validers["$key"],
                 array("options" => array("regexp" => $arr_regexp_validers["$key"]))
-                )){
-                }else{
-                    $arr_errors["$key"] = "$key n'est pas valide";
-                    echo '<pre>';
-                    print_r($arr_errors);
-                    $is_form_valid = false;
-                }
+            )){
+                $arr_errors["$key"] = "$key n'est pas valide";
+                echo '<pre>';
+                print_r($arr_errors);
+                $is_form_valid = false;
             }
-            if($is_form_valid){
-                // Action to do if all validation step passed
-                // redirection to the ok pg
-                redirect("../../valid-form.php");
-                
-                
-            } else {
-                echo 'going to NOT valid';
-                // if the form isn't valid
-                // Send back users to the form
+        }
+        if ($is_form_valid) {
+            // Action to do if all validation step passed
+            
+            
+            // redirection to the valid send page if everything went good
+            redirect("../../valid-form.php");
+            
+        } else {
+            echo 'going to NOT valid';
+            // if the form isn't valid
+            // Send back users to the form
+            redirect("../../contact.php");
+
+            
 
 
-            }
+            
+        }
     } else {
-        // à faire renvoyer page de "validation OK" pour le bot suite au honeypot
-        
+        // Honey Pot was taken thus we redirect to the valid-form page
+        redirect("../../valid-form.php");
     }
 }
 
