9c5c56d397d11520e3bdffd3e747254488eced75
Térence <terencehecq@gmail.com>
2019-12-05 10:26:08 +0100

    change cards on products

--- assets/php/card.php
+++ assets/php/card.php
@@ -2,12 +2,14 @@
 
 function generate_card($img_file_name, $title, $content, $link = "#"){
 
-echo    "<div class='card col-12 col-sm-5 col-md-3 m-2 p-0''>
+echo    "<div class='card col-12 col-sm-5 col-lg-3 m-3 p-0''>
             <img src='assets/img/$img_file_name' class='card-img-top' alt='$title'>
-            <div class='card-body'>
-                <h5 class='card-title font-weight-bold'>$title</h5>
-                <p class='card-text'>$content</p>
-                <a href='$link' class='btn btn-primary d-block bg-color'>Acheter</a>
+            <div class='card-body d-flex flex-column justify-content-between'>
+                <div>
+                    <h5 class='card-title font-weight-bold'>$title</h5>
+                    <p class='card-text'>$content</p>
+                </div>
+                <a href='$link' class='btn btn-primary d-block bg-color mt-2' target='_blank'>Acheter</a>
             </div>
         </div>";
 }
